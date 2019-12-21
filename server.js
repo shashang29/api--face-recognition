171fa2593b59e736016eb11657b60e02ec340fb7
Shashang <51124608+shashang29@users.noreply.github.com>
2019-12-21 16:45:29 +0545

    changed database

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-        host: '127.0.0.1',
+        host: 'postgresql-closed-58473',
         user: 'shashang',
         password: '',
         database: 'smart-brain'
